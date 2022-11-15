27510e9eceae1b3e26eea111f95b2406b508acd9
Dmitry Kovalev <doke174@yandex.ru>
2022-11-15 14:51:28 +0500

    fix: url

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,5 +1,5 @@
 export const apiConfig = {
-  baseUrl: 'https://api.kovalev.nomoredomains.icu',
+  baseUrl: 'http://api.kovalev.nomoredomains.icu',
 };
 
 //token: '4b6a8056-03c7-4003-80a9-a26f77782524'
